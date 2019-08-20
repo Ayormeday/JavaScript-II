51dee94688c5bfb5af41ee61ce381345fc7030e9
Ayomide Jones <ayormeday@gmail.com>
2019-08-20 13:03:02 +0100

    solved the first call back problem

--- assignments/callbacks.js
+++ assignments/callbacks.js
@@ -41,6 +41,7 @@ const items = ['Pencil', 'Notebook', 'yo-yo', 'Gum'];
 
 function getLength(arr, cb) {
   // getLength passes the length of the array into the callback.
+  cb (array.length);
 }
 
 function last(arr, cb) {
