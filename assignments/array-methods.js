f3dad5bada93d68e9842bae6a4a7269dbd813bc0
Ayomide Jones <ayormeday@gmail.com>
2019-08-20 15:57:11 +0100

    soome stretch added

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -94,10 +94,18 @@ const ticketPriceTotal = runners.reduce((total, person) => {
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
-// Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
+// Now that you have used .forEach(), .map(), .filter(), and .reduce().  
+// I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  
+// Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
-// Problem 1
+// Problem 1 want to return first name, last name and shirt size for record purpose; 
+const runnersInfoAndShirtsize = runners.filter((person) => {
+  return (`${person.first_name} ${person.last_name} ${person.shirt_size}`)
+});
+console.log(runnersInfoAndShirtsize);
+
 
 // Problem 2
+
 
 // Problem 3
